4973a71662167aebbedacb592add62c84fc9cf74
Keshav Sandhu <91087138+Sand68@users.noreply.github.com>
2022-06-16 19:15:10 -0400

    About us finished (#4)

--- src/about-us.php
+++ src/about-us.php
@@ -39,15 +39,27 @@
       </form>
     </div>
   </nav>
+  
+  <main>
+    <div class="jumbotron text-center">
+      <h1>About Us!</h1>
+    </div>
+    <div class="container">
+    	
+      <p>An LGBTQIA+ website is designed and analyzed for Ten Oaks Project company on the basis of functionality, friendly user interface, and cost efficiency to help users in Ottawa to get the LGBTQIA+ resources in less time cost. Furthermore, users can also download the information from a large database such as addresses of events or organizations, hotlines, or specific contactors of an LGBTQIA+ organization. The website also contains the functionality to redirect the user to the organization’s home page in case they are looking for more than contact information. The goal is to help users get better knowledge about LGBTQIA+ and promote the development of diverse communities. Basically, the user can download massive information about LBGTQIA+ and other resources about events and organizations. Although people from all over the Internet can access our website, the targeting group of users for this website will be people in the Ottawa area. In addition, only LGBT organizations in Ottawa will be mentioned on the website. Besides, our project will not be available on mobile platforms like iOS and Android. </p>
+    	<p> The LGBT community faces tremendous difficulties growing up in a society where heterosexuality is the only acceptable orientation and homosexuality is regarded as deviant. Studies have shown that violence and discrimination toward the LGBTQ+ community have resulted in many individuals under the influence of various fears, hiding their sexual orientation. However, they are still exposed to discrimination, psychological and/or physical violence. Particularly young LGBT community members who experience estrangement from family and friendship networks, harassment at school, and invisibility, which have led to underachievement at school, school drop-out, mental ill-health, and homelessness. This discrimination not only denies LGBT Community members equal access to key social goods, such as employment, health care, education, and housing, but it also marginalizes them in society and makes them one of the vulnerable groups who are at risk of becoming socially excluded. The LGBT community lacks support, guidance, and other resources. </p>
+    	
+    </div>
+    </main>
 
   <footer class="text-muted">
     <div class="container">
-      <p class="float-right"> 
+    	<p>Ten Oaks Link</p>
+     	 <p class="float-right"> 
         <i class="fa-brands fa-twitter-square"></i> &nbsp; 
         <i class="fa-brands fa-instagram"></i> &nbsp; 
         <i class="fa-brands fa-facebook-square"></i> &nbsp; 
       </p>
-      <p>Ten Oaks Link</p>
     </div>
   </footer>
 </body>
